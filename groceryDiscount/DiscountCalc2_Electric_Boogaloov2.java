abba0202683ed845c40c3e31896be80a2adc0796
bmerola512 <55250416+bmerola512@users.noreply.github.com>
2020-02-24 13:31:01 -0500

    Add files via upload
    This is my actual submission for groceryDIscount v2

---
+++ groceryDiscount/DiscountCalc2_Electric_Boogaloov2.java
@@ -0,0 +1,62 @@
+package groceryDiscount;
+
+import java.util.Scanner;
+
+public class DiscountCalc2_Electric_Boogaloov2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double discount = 0;
+// user input            
+        System.out.println("Please enter the cost of your groceries. To end enter 0 or a non-number: ");
+        
+            while (in.hasNextDouble()) {
+                double spent = in.nextDouble();
+                
+// if and buts               
+                if (spent >= 10 && spent <= 1000) {
+                    if (spent >= 10 && spent <= 60) {
+                        
+                    	discount = 0.08 * spent;
+                        System.out.printf("You win a discount coupon of $%.2f (8 percent of your purchase). " , discount);
+                        System.out.println("Enter another amount or 0 to end");
+                    }
+                    
+                    else if (spent > 60 && spent <= 150) {
+                        
+                    	discount = 0.1 * spent;
+                        System.out.printf("You win a discount coupon of $%.2f (10 percent of your purchase). " , discount);
+                        System.out.println("Enter another amount or 0 to end");
+                    }
+                    
+                    else if (spent > 150 && spent <= 210) {
+                        
+                    	discount = 0.12 * spent;
+                        System.out.printf("You win a discount coupon of $%.2f (12 percent of your purchase). " , discount);
+                        System.out.println("Enter another amount or 0 to end");
+                    }
+                    
+                    else if (spent > 210) {
+                        
+                    	discount = 0.14 * spent;
+                        System.out.printf("You win a discount coupon of $%.2f (14 percent of your purchase). " , discount);
+                        System.out.println("Enter another amount or 0 to end");
+                    }
+                }
+                
+                else if (spent == 0) {
+                    
+                	System.out.println("Exiting");
+                    break;
+                }
+                
+                else {
+                    System.out.println("Amount has to be within $10 and $1000 to be eligible. Enter another amount or 0 to end.");
+                }
+
+            }
+            
+            System.out.println("Exited program");
+
+    }
+}
